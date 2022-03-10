41f40a0c05538dab3d593cd2b4b4042a5a3c56ff
betty0430 <82023103+betty0430@users.noreply.github.com>
2022-03-10 08:59:17 +0800

    Add files via upload

---
+++ 0309Program2 (1).cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int[,] pt = new int[6, 3];
+                pt[0, 0] = 123; pt[0, 1] = 456; pt[0, 2] = 9000;
+                pt[1, 0] = 456; pt[1, 1] = 789; pt[1, 2] = 5000;
+                pt[2, 0] = 789; pt[2, 1] = 888; pt[2, 2] = 6000;
+                pt[3, 0] = 336; pt[3, 1] = 558; pt[3, 2] = 10000;
+                pt[4, 0] = 775; pt[4, 1] = 666; pt[4, 2] = 12000;
+                pt[5, 0] = 566; pt[5, 1] = 221; pt[5, 2] = 7000;
+                string[] a = Console.ReadLine().Split(' ');
+                bool c = false;
+                int who_x = 0;
+                for (int b = 0; b < 6; b++)
+                {
+                    if ((pt[b, 0] == int.Parse(a[0])) && (pt[b, 1] == int.Parse(a[1])))
+                    {
+                        c = true; who_x = b;
+                        Console.WriteLine("OK");
+                        break;
+                    }
+                }
+
+                if (c == false)
+                {
+                    Console.WriteLine("ERROR");
+                }
+                else
+                {
+                    while (true)
+                    {
+                        string[] d = Console.ReadLine().Split(' ');
+                        switch (d[0])
+                        {
+                            case "L":
+                                if (int.Parse(d[1]) < pt[who_x, 2])
+                                {
+                                    pt[who_x, 2] -= int.Parse(d[1]);
+                                    Console.WriteLine("OK");
+                                }
+                                else Console.WriteLine("ERROR");
+                                break;
+                            case "S":
+                                Console.WriteLine(pt[who_x, 2]);
+                                break;
+                            case "E":
+                                Console.WriteLine("BYE");
+                                break;
+                            default:
+                                Console.WriteLine("ERROR");
+                                break;
+                        }
+                        if (d[0] == "E") break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 0309Program2 (2).cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //(1≤ n ≤ 10)
+                string a = Console.ReadLine();
+                if (int.Parse(a) < 1 || int.Parse(a) > 10) break;
+                string[] d = new string[int.Parse(a)];
+                bool pt1 = false;
+                for(int b = 1; b <= int.Parse(a); b++)
+                {
+                    string c = Console.ReadLine();
+                    if (c.Length >= 100||c=="")
+                    {
+                        pt1 = true;
+                        break;
+                    }
+                    d[b - 1] = c;
+                }
+                if (pt1 == true) break;
+                else
+                {
+                    int i = 0;
+                    for(int e = 0; e < int.Parse(a); e++)
+                    {
+                        string[] pt2 = new string[d[e].Length];
+                        int[] pt3 = new int[d[e].Length];
+                        i = 0;
+                        for (int f = 0; f < d[e].Length; f++)
+                        {
+                            if (f == 0)
+                            {
+                                pt2[i] = d[e].Substring(f, 1);
+                                pt3[i]+=1;
+                            }
+                            else
+                            {
+                                bool pt5 = false;
+                                for (int g = 0; g <= i; g++)
+                                {
+                                    if (d[e].Substring(f, 1) == pt2[g])
+                                    {
+                                        pt3[g]+=1;
+                                        pt5 = true;
+                                        break;
+                                    }
+                                }
+                                if (pt5 == false)
+                                {
+                                    i++;
+                                    pt2[i] = d[e].Substring(f, 1);
+                                    pt3[i]+=1;
+                                }
+                            }
+                        }
+                        int big = -1;
+                        for(int p = 0; p < pt3.Length; p++)
+                        {
+                            if (pt3[p] > big)
+                            {
+                                big = pt3[p];
+                            }
+                        }
+                        for (int p = 0; p < pt3.Length; p++)
+                        {
+                            if (pt3[p] == big)
+                            {
+                                Console.WriteLine(pt2[p]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
